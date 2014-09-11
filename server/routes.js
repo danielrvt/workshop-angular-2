161b2fbe2fa980b23d6dd2f40f34689028fc271a
Daniel Rodriguez del Villar Trimarchi <dani.tri88@gmail.com>
2014-09-11 10:49:59 -0430

    [Productos]
    * Agregado dummys de productos

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/products', require('./api/product'));
   app.use('/api/sessions', require('./api/session'));
   app.use('/api/things', require('./api/thing'));
   
